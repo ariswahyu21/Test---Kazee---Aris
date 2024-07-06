85f471f7a3ea7337891bef93704fea77c8f0c4d3
WAGYUA5\62895 <arwah712@gmail.com>
2024-07-06 13:21:57 +0700

    Buat File Create untuk Menambahkan Data

---
+++ resources/views/employees/create.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Employee Management</title>
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+</head>
+
+<body>
+    <div class="container">
+        <h2>Create New Employee</h2>
+
+        @if ($errors->any())
+        <div class="alert alert-danger">
+            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+            <ul>
+                @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+        @endif
+
+        <form action="{{ route('employees.store') }}" method="POST">
+            @csrf
+
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Name:</strong>
+                        <input type="text" name="name" class="form-control" placeholder="Name">
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Email:</strong>
+                        <input type="email" name="email" class="form-control" placeholder="Email">
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Position:</strong>
+                        <input type="text" name="position" class="form-control" placeholder="Position">
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</body>
+
+</html>
